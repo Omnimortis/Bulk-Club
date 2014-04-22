cc7a51d4bd780b92fac2d23e64079140017e882d
mclark47 <mclark47@CSLAB-103.student.saddleback.edu>
2014-04-22 09:13:46 -0700

    Hello World

---
+++ Bulk Club/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 22, 2014
+ *      Author: mclark47
+ */
+//Matt Clark
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+
+    return 0;
+}
+
